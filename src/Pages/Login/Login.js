54f9bf8031f106abcd7f1bfdc679215c6246b3a4
shariarhossain23e <shariarhossain23@gamil.comm>
2022-04-17 01:47:16 +0600

    login and signup style

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
+import { FaFacebook, FaGoogle } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
@@ -21,11 +23,26 @@ const Login = () => {
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" />
               </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-              <Button>Login</Button>
+              <div className="text-center">
+                <Button className="w-100  ">Login</Button>
+              </div>
             </Form>
+            <p className="fs-6 mt-4 text-center">You are new? <Link to='/signup'>Signup</Link></p>
+            <div className="d-flex align-items-center mt-4">
+              <div
+                style={{ height: "1px", border: "1px solid #cccccc" }}
+                className="w-50"
+              ></div>
+              <p className="mt-2 mx-1">or</p>
+              <div
+                style={{ height: "1px", border: "1px solid #cccccc" }}
+                className="w-50"
+              ></div>
+            </div>
+            <div className="text-center">
+              <FaFacebook className="icon text-primary mx-2"></FaFacebook>
+              <FaGoogle className="icon google-Icon"></FaGoogle>
+            </div>
           </div>
         </div>
       </div>
--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { FaFacebook, FaGoogle } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
     return (
         <div>
-           
+            <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-md-6 mx-auto form-container">
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+              </Form.Group>
+              <div className="text-center">
+                <Button className="w-100  ">Login</Button>
+              </div>
+            </Form>
+            <p className="fs-6 mt-4 text-center">You have Already Account? <Link to='/login'>Login</Link></p>
+            <div className="d-flex align-items-center mt-4">
+              <div
+                style={{ height: "1px", border: "1px solid #cccccc" }}
+                className="w-50"
+              ></div>
+              <p className="mt-2 mx-1">or</p>
+              <div
+                style={{ height: "1px", border: "1px solid #cccccc" }}
+                className="w-50"
+              ></div>
+            </div>
+            <div className="text-center">
+              <FaFacebook className="icon text-primary mx-2"></FaFacebook>
+              <FaGoogle className="icon google-Icon"></FaGoogle>
+            </div>
+          </div>
+        </div>
+      </div>
         </div>
     );
 };
